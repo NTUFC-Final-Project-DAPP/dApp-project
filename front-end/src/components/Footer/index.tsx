d4c75095a49ab3b23ceac9e0ec17b980f3d001cc
Frisk <79501315+Frisk0316@users.noreply.github.com>
2024-06-21 17:51:19 +0800

    Add files via upload

---
+++ front-end/src/components/Footer/index.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return (
+      <footer className="p-4 bg-opacity-20 bg-white">
+        <div className="container mx-auto text-center">
+          <p className="text-white font-bold">&copy; {new Date().getFullYear()}   Team member: 姚心柔、謝宗佑、林奕璇、Maxwell Friedman</p>
+        </div>
+      </footer>
+    );
+  }
+  
